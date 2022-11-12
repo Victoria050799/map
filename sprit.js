a68a032f12f327c01a6f3f17a7f640bec99be367
Victoria050799 <balakaeva99qw@mail.ru>
2022-11-12 18:01:59 +0300

    this

--- sprit.js
+++ sprit.js
@@ -48,7 +48,7 @@ const arr = [1, 2, 3, 4];
 console.log(IndexOf(2))*/
 
 
-function Includes(searchElement,fromIndex ) {
+/*function Includes(searchElement,fromIndex ) {
   fromIndex=0;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === searchElement) {  
@@ -59,4 +59,92 @@ function Includes(searchElement,fromIndex ) {
 };
 
 const arr = [1, 2, 3, 4];
-console.log(Includes(2))
+console.log(Includes(2))*/
+/*
+function Concat(valueN,Array){
+  for (let i = 0; i < arr.length; i++){
+  return [ valueN.push(Array)];}
+}
+const arr = [1, 2, 3, 4];
+const arr2=[5,6,7,8];
+
+console.log(Concat(arr,arr2))*/
+
+Вопрос 1.
+
+const call = {
+  caller: "mom", 
+  says: function() {
+    console.log(`Hey, ${this.caller} just called.`);
+  }
+};
+
+call.says();
+
+Что приведенный выше код будет выводить на консоль?
+
+(A) Hey, undefined just called.
+(B) Hey, mom just called.// ответ B 
+(C) Hey, caller just called.
+
+Вопрос 2.
+
+const call = {
+  caller: "mom", 
+  says: () => {
+    console.log(`Hey, ${this.caller} just called.`);
+  }
+};
+
+call.says();
+
+Что приведенный выше код будет выводить на консоль?
+
+(A) Hey, undefined just called.//a
+(B) Hey, mom just called.
+(C) Hey, caller just called.
+
+Вопрос 3.
+
+const call = {
+  caller: "mom", 
+  says: function() {
+    console.log(`Hey, ${this.caller} just called.`);
+  }
+};
+
+let newCall = call.says;
+
+newCall();
+
+Что приведенный выше код будет выводить на консоль?
+
+(A) Hey, undefined just called.//a
+(B) Hey, mom just called.
+
+
+Вопрос 4.
+
+function anotherCaller() {
+  console.log(`${this.caller} called, too!`);
+}
+
+const call = {
+  caller: "mom", 
+  anotherCaller: anotherCaller,
+  says: function() {
+    console.log(`Hey, ${this.caller} just called.`);
+  }
+};
+
+let newCall = call.anotherCaller;
+
+newCall();
+
+
+
+Что приведенный выше код будет выводить на консоль?
+
+(A) mom called, too!
+(B) Hey, mom just called.
+(C) undefined called, too!//c
